6126c26ac23da24d773bdd32ec20698ab304dfcc
Carl Törnqvist <carl@tornqv.ist>
2019-06-24 18:17:15 +0200

    Fix preview link

--- components/prismic-toolbar/index.js
+++ components/prismic-toolbar/index.js
@@ -17,7 +17,7 @@ module.exports = class PrismicToolbar extends Component {
       enabled: typeof window !== 'undefined'
     }
 
-    this.href = () => state.origin + '/api/prismic-preview'
+    this.href = () => state.origin + state.href
 
     if (state.query.token && typeof window !== 'undefined') {
       let expires = process.env.NODE_ENV === 'development'
