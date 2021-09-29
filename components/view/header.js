c3c8dda867da605c1cbfc124115ae4c056ac4970
AntonTrollback <anton@trollback.se>
2021-09-29 18:17:42 +0200

    Add flag to header

--- components/view/header.js
+++ components/view/header.js
@@ -26,6 +26,10 @@ function header (items, href) {
                 </a>${(index + 1) < items.length ? html`<span class="u-hiddenVisually">,</span>` : null}
               </span>
             `)}
+            <a class="View-link View-link--light View-link--flag" href="https://sv.yennengaprogress.se">
+              <svg role="presentation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 10"><path fill="#006aa7" d="M0,0H16V10H0Z"/><path fill="#fecc00" d="M0,4H5V0H7V4H16V6H7V10H5V6H0Z"/></svg>
+              Sidan på svenska
+            </a>
           </nav>
         </div>
       </div>
